439bf0a0cbbcaa90bc39437dcfffe6722d0aec20
jacob koskimaki <jkoskimaki@gmail.com>
2020-06-04 20:32:04 -0600

    controllers

---
+++ Doctors_App_Notes.rb
@@ -0,0 +1,12 @@
+# Create a new rails app and a git repo.
+# Create models (hint: users and doctors have many appointments, a user should have many doctors THROUGH appointments, etc.)
+# Add model validations 
+# Create controllers that manage all CRUD actions for each model.
+# Create Views.
+
+
+# You will create a rails application that manages a user's doctor appointments. A user will have many doctors through appointments. You will be experimenting with advanced relationships in your models using has_many through associations.
+
+gem 'pry'
+gem 'faker'
+gem 'semantic-ui-sass'
