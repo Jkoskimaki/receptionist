c52552df85c9cbc8b237163b15381c7658a849fe
Jacob Koskimaki <jkoskimaki@gmail.com>
2020-06-09 22:00:34 -0600

    latest

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,8 +1,9 @@
 class PatientsController < ApplicationController
-  before_action :set_patient, only: [:show, :destroy]
+  before_action :set_patient, only: 
+  [:show, :edit, :update, :destroy]
 
   def index
-    @patient = Patient.all
+    @patients = Patient.all
   end
 
   def show
@@ -21,18 +22,29 @@ class PatientsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @patient.update(patient_params)
+      redirect_to @patient
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @patient.destroy
     redirect_to patients_path
   end
 
   private
-    def set_patient
-      @patient = Patient.find(params[:id])
-    end
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
 
     def patient_params
-      params.require(:course).permit(:first_name, :last_name)
+      params.require(:patient).permit(:first_name, :last_name)
     end
 
 end
